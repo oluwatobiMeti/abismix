5fd61348ab00df6b0cdd66613914d77aaa3ed612
Oluwatobi metibemu <metibemuoluwatobi2@gmail.com>
2023-07-16 15:43:48 +0100

    initial commit

---
+++ src/components/Clickable.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Clickable() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Clickable
---
+++ src/components/RoundImage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RoundImage() {
+  return (
+    <img src="" alt="" srcset="" />
+  )
+}
+
+export default RoundImage;
